e16a6b7c68b48fe16fe8e568539fe9c659a15a46
k1sihyeon <tlgus530@naver.com>
2024-09-11 15:37:20 +0900

    24.09.11

---
+++ chat/client.c
@@ -0,0 +1,126 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define TCP_PORT 5100
+
+typedef struct message {
+    int code;
+    char id[BUFSIZ];
+    char name[BUFSIZ];
+    char mesg[BUFSIZ];
+} Message;
+
+void sigfunc(int no) {
+    printf("Signal from child : %d\n", no);
+    exit(0);
+}
+
+int main(int argc, char** argv) {
+    pid_t pid;
+    struct sockaddr_in servaddr;
+
+    int ssock;
+    int port = TCP_PORT;
+
+    char mesg[BUFSIZ];
+
+    /////////////////////////////////////
+
+    if (argc < 2) {
+        printf("Usage : %s <IP ADRESS>\n", argv[0]);
+        return -1;
+    }
+
+    if (argc == 3) {
+        port = atoi(argv[2]);
+    }
+
+    signal(SIGCHLD, sigfunc);
+
+    // socket 생성 - TCP (STREAM)
+    if ((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket()");
+        return -1;
+    }
+
+    // 서버 주소 초기화
+    memset(&servaddr, 0, sizeof(servaddr));
+    // 명령어 인수의 string IP주소를 servaddr에 네트워크 형식으로 변환
+    inet_pton(AF_INET, argv[1], &(servaddr.sin_addr.s_addr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+
+    // 접속
+    if (connect(ssock, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+        perror("connect()");
+        return -1;
+    }
+
+    if ((pid = fork()) < 0) {
+        perror("fork()");
+        return -1;
+    }
+    else if (pid == 0) {  // Child Process - 서버로부터 받은 메시지를 출력
+        
+        char buf[BUFSIZ];
+        //
+
+        while (true) {
+            Message* msgb = (Message *)malloc(sizeof(Message));
+            memset(buf, 0, BUFSIZ);  // mesg 지우기
+            memset(msgb, 0, sizeof(Message));
+            memset(msgb->id, 0, BUFSIZ);
+            memset(msgb->name, 0, BUFSIZ);
+            memset(msgb->mesg, 0, BUFSIZ);
+
+            // 서버 -> 소켓 -> mesg
+            if (recv(ssock, msgb, sizeof(Message), 0) <= 0) {
+                perror("recv()");
+                return -1;
+            }
+
+            // mesg 출력
+            printf("%s[%s]: %s", msgb->id, msgb->name, msgb->mesg);
+            fflush(stdout);
+
+            free(msgb);
+        }
+    }
+    else if (pid > 0) {  // Parent Process - 사용자가 입력한 메시지를 서버로 전송
+        char buf[BUFSIZ];
+
+        while (true) {
+            memset(buf, 0, BUFSIZ);  // mesg 지우기
+
+            // 사용자 메시지 키보드 입력
+            fgets(buf, BUFSIZ, stdin);
+
+            // 메시지를 소켓으로 변환 후 전송
+            if (send(ssock, buf, BUFSIZ, MSG_DONTWAIT) <= 0) {
+                perror("send()");
+                return -1;
+            }
+        }
+    }
+    else {
+        perror("fork()");
+        return -1;
+    }
+
+    // 쓰기 소켓 닫음
+    shutdown(ssock, SHUT_WR);
+
+    // 소켓 닫기
+    close(ssock);
+
+    return 0;
+}
---
+++ chat/server.c
@@ -0,0 +1,329 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define TCP_PORT 5100
+#define MAX_CLIENT 10
+
+static int CHILD_COUNT = 0;
+
+// client
+// 1. 사용자 지정 이름, 색상 설정
+// 2. 메시지 입력, 전송
+// 3. 다른 client 메시지 수신, 출력 -> fork
+
+typedef struct user {
+    char id[BUFSIZ];
+    char pw[BUFSIZ];
+    char name[BUFSIZ];
+} User;
+
+typedef struct message {
+    int code;
+    char id[BUFSIZ];
+    char name[BUFSIZ];
+    char mesg[BUFSIZ];
+} Message;
+
+void sigfunc(int no) {
+    printf("Signal from child(%d) : %d\n", CHILD_COUNT, no);
+    CHILD_COUNT -= 1;
+
+    if (CHILD_COUNT == 0)
+        exit(0);
+}
+
+void Send(int sockfd, const void *buf, User* user) {
+    Message* msg = (Message*)malloc(sizeof(Message));
+    memset(msg, 0, sizeof(Message));
+    memset(msg->id, 0, BUFSIZ);
+    memset(msg->name, 0, BUFSIZ);
+    memset(msg->mesg, 0, BUFSIZ);
+
+    strcpy(msg->mesg, (char*)buf);
+    msg->code = 0;
+
+    if (user == NULL) {
+        strcpy(msg->id, "server");
+        strcpy(msg->name, "server");
+    }
+    else {
+        strcpy(msg->id, user->id);
+        strcpy(msg->name, user->name);
+    }
+
+    size_t n = send(sockfd, msg, sizeof(Message), MSG_DONTWAIT);
+    usleep(100);
+    free(msg);
+
+    if (n <= 0) {
+        perror("Send()");
+        exit(0);
+    }
+
+    printf("Send data : %s", (char*)buf);
+}
+
+int main(int argc, char **argv) {
+    pid_t pid;
+    socklen_t clen;
+    struct sockaddr_in servaddr, cliaddr;
+
+    FILE *csv_fp;
+    sem_t *sem;
+
+    int n;
+    int port = TCP_PORT;
+    int ssock;
+
+    char mesg[BUFSIZ];
+
+    //////////////////////////////
+
+    sem = sem_open("login_sem", O_CREAT, 0644, 1);
+
+    if (argc == 2) {
+        port = atoi(argv[1]);
+    }
+
+    // child proc. signal 처리 설정
+    signal(SIGCHLD, sigfunc);
+
+    // server tcp socket 생성
+    if ((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket()");
+        return -1;
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port);
+
+    if (bind(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind()");
+        return -1;
+    }
+
+    if (listen(ssock, MAX_CLIENT) < 0) {
+        perror("listen()");
+        return -1;
+    }
+
+    clen = sizeof(cliaddr);
+
+    do {
+        int n;
+        char ip[BUFSIZ];
+        int csock;
+
+        User user;
+        memset(&user, 0, sizeof(User));
+
+        clen = sizeof(cliaddr);
+        csock = accept(ssock, (struct sockaddr *)&cliaddr, &clen);
+        inet_ntop(AF_INET, &cliaddr.sin_addr, ip, BUFSIZ);
+        printf("Client is connected : %s\n", ip);
+        CHILD_COUNT += 1;
+
+        if ((pid = fork()) < 0)  ///////////////////////  fork  //////////////////////////////
+            perror("fork()");
+        else if (pid == 0) {  // Child Proc. - 클라이언트가 연결될 때 마다
+            close(ssock);
+
+            // 로그인, 사용자 이름 설정
+            while (true) {
+                int num;
+                char buf[BUFSIZ];
+
+                printf("after fork\n");
+                fflush(stdout);
+
+                Send(csock, "\033[2J\033[1;1H", NULL);
+                Send(csock, "Welcome to chat server\n", NULL);
+                Send(csock, "=======================\n", NULL);
+                Send(csock, "===== Select Menu =====\n", NULL);
+                Send(csock, "=======================\n", NULL);
+                Send(csock, "1. Login\n", NULL);
+                Send(csock, "2. Register\n", NULL);
+                Send(csock, "3. Exit\n", NULL);
+                Send(csock, "=======================\n", NULL);
+                Send(csock, "Input >> ", NULL);
+
+                read(csock, buf, BUFSIZ);
+                num = atoi(buf);
+
+                fflush(stdin);
+
+                if (num == 1) {
+                    char uid[BUFSIZ];
+                    char upw[BUFSIZ];
+
+                    char fid[BUFSIZ];
+                    char fpw[BUFSIZ];
+                    char fname[BUFSIZ];
+
+                    bool isLogin = false;
+
+                    Send(csock, "\033[2J\033[1;1H", NULL);
+                    Send(csock, "Input ID >>  ", NULL);
+                    n = read(csock, uid, BUFSIZ);
+                    uid[n] = '\0';
+                    strcpy(strtok(uid, "\n"), uid);
+
+                    Send(csock, "Input Password >> ", NULL);
+                    n = read(csock, upw, BUFSIZ);
+                    upw[n] = '\0';
+                    strcpy(strtok(upw, "\n"), upw);
+
+                    // csv 파일(세마포어 필요)로 로그인 유효성 검사
+                    sem_wait(sem);
+                    csv_fp = fopen("login.csv", "r");
+
+                    // 사용자 정보 검색
+                    while (fscanf(csv_fp, "%s, %s, %s\n", fid, fpw, fname) != EOF) {
+                        if (!strcmp(uid, fid) && !strcmp(upw, fpw)) {
+                            isLogin = true;
+                            break;
+                        }
+                    }
+                    fclose(csv_fp);
+                    sem_post(sem);
+
+                    if (isLogin) {
+                        Send(csock, "Login Success !!\n", NULL);
+                        strcpy(user.id, uid);
+                        strcpy(user.pw, upw);
+                        strcpy(user.name, fname);
+                        break;
+                    }
+                    else {
+                        Send(csock, "Login Failed !!\n", NULL);
+                        continue;
+                    }
+                }
+                else if (num == 2) {
+                    char buf[BUFSIZ];
+
+                    char uid[BUFSIZ];
+                    char upw[BUFSIZ];
+                    char uname[BUFSIZ];
+
+                    Send(csock, "\033[2J\033[1;1H", NULL);
+                    Send(csock, "Input ID >>  ", NULL);
+                    n = read(csock, buf, BUFSIZ);
+                    buf[n] = '\0';                 // null 종료
+                    strcpy(uid, strtok(buf, "\n"));  // 개행 문자 제거
+                    memset(buf, 0, BUFSIZ);
+
+                    Send(csock, "Input Password >> ", NULL);
+                    n = read(csock, buf, BUFSIZ);
+                    buf[n] = '\0';                   // null 종료
+                    strcpy(upw, strtok(buf, "\n"));  // 개행 문자 제거
+                    memset(buf, 0, BUFSIZ);
+
+                    Send(csock, "Input Name >> ", NULL);
+                    n = read(csock, buf, BUFSIZ);
+                    buf[n] = '\0';                     // null 종료
+                    strcpy(uname, strtok(buf, "\n"));  // 개행 문자 제거
+                    memset(buf, 0, BUFSIZ);
+
+                    // csv 파일(세마포어 필요)로 회원가입
+                    sem_wait(sem);
+
+                    csv_fp = fopen("login.csv", "a");
+                    fprintf(csv_fp, "%s, %s, %s\n", uid, upw, uname);
+                    fclose(csv_fp);
+
+                    sem_post(sem);
+
+                    strcpy(user.id, uid);
+                    strcpy(user.pw, upw);
+                    strcpy(user.name, uname);
+
+                    Send(csock, "Register Success !!\n", NULL);
+                    break;
+                }
+                else if (num == 3) {
+                    close(csock);
+                    goto CLOSE;  // 해당 클라이언트 listen 종료
+                }
+                else {
+                    Send(csock, "\033[2J\033[1;1H", NULL);
+                    Send(csock, "Invalid input !!\n", NULL);
+                    getchar();
+                    continue;
+                }
+            }
+
+            ///// todo: 채팅 그룹 설정
+
+            // 최대 접속자 제한?
+            // 동시 접속시 -> 세마포어?
+
+            // 채팅
+
+            do {
+                memset(mesg, 0, BUFSIZ);
+
+                if ((n = read(csock, mesg, BUFSIZ)) <= 0)
+                    perror("read()");
+
+                mesg[n] = '\0';
+
+                printf("Received data : %s", mesg);
+
+                Message* msg;
+                msg = (Message*)malloc(sizeof(Message));
+                memset(msg, 0, sizeof(Message));
+                memset(msg->id, 0, BUFSIZ);
+                memset(msg->name, 0, BUFSIZ);
+                memset(msg->mesg, 0, BUFSIZ);
+
+                msg->code = 0;
+                strcpy(msg->id, user.id);
+                strcpy(msg->name, user.name);
+                strcpy(msg->mesg, mesg);
+
+                printf("Message created : %s\n", msg->mesg);
+
+                Send(csock, msg, &user);
+
+                // 다른 client에도 write -> broadcast
+                // client 접속 정보 (csock, cliaddr등) 저장하기 위한 배열 필요
+                // 공유메모리 등 ipc 사용 필요
+
+                // !list, 리스트, 귓속말, 강조 등 기능 추가
+                // 파일 전송?
+                
+                free(msg);
+            } while (strncmp(mesg, "q", 1));
+        }
+        else if (pid > 0) {  // Parent Proc.
+            close(csock);
+
+            // 클라이언트들이 보내는 메시지 read 대기
+        }
+        else {
+            perror("fork()");
+        }
+
+        close(csock);
+
+    } while (strncmp(mesg, "q", 1));
+
+CLOSE:;
+    sem_close(sem);
+    sem_unlink("login_sem");
+    close(ssock);
+
+    return 0;
+}
